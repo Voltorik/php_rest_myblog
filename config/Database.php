7425d5c05dc22caef21ce5cbf9957a0af7ab2e97
Jordan Tyler <jmattyler@hotmail.com>
2023-02-25 15:02:34 -0600

    Changed DB conn to PostgreSQL DB

--- config/Database.php
+++ config/Database.php
@@ -2,23 +2,24 @@
   class Database {
     // DB Params
     private $host = 'localhost';
+    private $port = 5432;
     private $db_name = 'myblog';
-    private $username = 'root';
-    private $password = '';
+    private $username = 'postgres';
+    private $password = 'postgres';
     private $conn;
 
     // DB Connect
     public function connect() {
       $this->conn = null;
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
-        $this->username, $this->password);
+        $this->conn = new PDO($dsn, $this->username, $this->password);
+
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
-
       return $this->conn;
     }
   }
